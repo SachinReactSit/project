fce5c3e081e556eba968782dca4f7d0b826d4129
Sachin Kavhale <sachuukavhale@gmail.com>
2025-06-16 17:12:10 +0530

    increment decrement using useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [a, setA] = useState(0);
   return (
     <div className="App">
       Hello 
+      <button onClick={()=>{setA(a+1)}}>For high</button>
+{a}
+      <button onClick={ ()=>{setA(a-1)}}>For low</button>
     </div>
   );
 }
